c00e2ed8bc86f37c46b58163aaab70fb3141fd7d
Rishi-Bhati <bhatirishi5@gmail.com>
2025-04-18 23:03:50 +0530

    feat: add CreateHackathon API endpoint and update requirements

--- hack_back/hackathon/views.py
+++ hack_back/hackathon/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import HackathonSession, ThemeSelection
 # Create your views here.
+
+class CreateHackathonView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    #recieve the pdf or plain text and send it to ai
+    def post(self, request):
+        """
+        Handle file upload and OCR processing
+        Steps:
+        1. Process text message if present
+        2. Process file if uploaded
+        3. Create chat messages and broadcast
+        """
+        pass
