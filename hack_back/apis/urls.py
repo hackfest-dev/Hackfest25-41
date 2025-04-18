b081822b97ce1e1aa6da7996ac4e84ebb06c994e
Rishi-Bhati <bhatirishi5@gmail.com>
2025-04-19 03:35:47 +0530

    `Added/removed include path for 'ideas.urls' in hack_back/apis/urls.py and modified hack_back/ideas/views.py`

--- hack_back/apis/urls.py
+++ hack_back/apis/urls.py
@@ -4,7 +4,7 @@ from django.urls import include
 urlpatterns = [
     path('auth/', include('users.urls') ),
     path('session/',include('hackathon.urls')),
-#     path('ideas/',include('ideas.urls')),
+    path('ideas/',include('ideas.urls')),
 #     path('chat/',include('chat.urls')),
 #     path('readme/',include('readme.urls')),
 #     path('pitch/',include('pitch.urls')),
--- hack_back/ideas/views.py
+++ hack_back/ideas/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import HackathonSession
+from core.pusher import pusher_client
+from core.theme_extractor import extract_themes_from_text
+import logging
+import io
+import json
 
 # Create your views here.
+
+
+class GenerateIdeasView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
