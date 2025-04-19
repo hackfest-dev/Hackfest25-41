ff0846ebb0f39d80d9dbf80a589378e595f810bf
Rishi-Bhati <bhatirishi5@gmail.com>
2025-04-19 22:59:00 +0530

    Add pitch generation API and integrate with URL routing

--- hack_back/pitch/views.py
+++ hack_back/pitch/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from core.scripts.auto_readme_parallel import generate_readme_for_repo
+from urllib.parse import urlparse
+import os
 
 # Create your views here.
+
+class GeneratePitchView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        pass
